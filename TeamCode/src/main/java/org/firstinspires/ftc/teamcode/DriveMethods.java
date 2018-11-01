0d429e09b409e65e5039b353991fe3d50c769525
avamaryp <32110899+avamaryp@users.noreply.github.com>
2018-11-01 17:00:51 -0400

    Update DriveMethods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMethods.java
@@ -5,33 +5,56 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * Created by shrey on 2017-11-05.
- */
 
 public class DriveMethods extends HardwarePushBot{
 
-        public static void driveRight(double speed){
-            rightFrontDrive.setPower(speed);
-            rightBackDrive.setPower(speed);
-        }
+    public static void driveRight(double speed){
+        rightFrontDrive.setPower(speed);
+        rightBackDrive.setPower(speed);
+    }
 
-        public static void driveLeft(double speed){
-            leftFrontDrive.setPower(speed);
-            leftBackDrive.setPower(speed);
-        }
+    public static void driveLeft(double speed){
+        leftFrontDrive.setPower(speed);
+        leftBackDrive.setPower(speed);
+    }
+
+    public static void startIntake(double speed){
+        intake.setPower(speed);
+
+    }
+
+    public static void liftUp(double speed){
+        lift.setPower(speed);
+    }
+
+    public static void liftDown(double speed){
+        lift.setPower(speed);
+    }
+
+    public static void gripUp(double speed){
+        grip.setPower(speed);
+    }
+
+    public static void gripDown(double speed){
+        grip.setPower(speed);
+    }
+
 
     /*
      * This method is used to stop the drive motors in order for the robot to stop when in between
      * the two thresholds. It's important to do this so the robot will stop, safely
      */
-        public static void stopRightMotors() { //always zero
-            rightFrontDrive.setPower(0);
-            rightBackDrive.setPower(0);
-        }
+    public static void stopRightMotors() { //always zero
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
 
-        public static void stopLeftMotors(){
-            leftFrontDrive.setPower(0);
-            leftBackDrive.setPower(0);
-        }
+    public static void stopLeftMotors(){
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+    }
+
+    public static void stopIntake(){
+        intake.setPower(0);
+    }
 }
