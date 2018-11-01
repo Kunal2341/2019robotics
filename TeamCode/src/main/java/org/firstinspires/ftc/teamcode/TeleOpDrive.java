ed41725e5255b3eadfb7b1cc66ccc490c901f9b3
avamaryp <32110899+avamaryp@users.noreply.github.com>
2018-11-01 17:01:31 -0400

    Update TeleOpDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -6,7 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @TeleOp(name = "Tel", group = "Sensor")
 public class TeleOpDrive extends LinearOpMode{
 
-    HardwarePushBot robot = new HardwarePushBot();
+    HardwarePushBot robot = new
+    HardwarePushBot();
 
 
     @Override
@@ -20,20 +21,30 @@ public class TeleOpDrive extends LinearOpMode{
         telemetry.update();
 
 
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
             // Drive variables
-            double rightPower = -gamepad1.right_stick_y * 0.8;
+            double rightPower = -gamepad1.right_stick_y * 0.8;  //what's the 0.8
             double leftPower = -gamepad1.left_stick_y * 0.8;
+            double liftPower = -gamepad2.right_stick_y * 0.8;
+            double gripPower = -gamepad2.left_stick_y * 0.8;
+
             double reverse_Thres = -0.3;
             double forward_Thres = 0.3;
-            double drive_Speed = 0.7;
+
+            double up_Thres = 0.3;
+            double down_Thres = -0.3;
+
+            double drive_Speed = 0.9;
             double turn_Speed = 0.5;
 
+            double intake_Speed = 0.5;         // mentions grip lift and intake
+            double lift_Speed = 0.5;
+
+            double grip_speed = 0.5;
 
             // Send calculated power to wheels
             // Drive system
@@ -55,9 +66,30 @@ public class TeleOpDrive extends LinearOpMode{
                 DriveMethods.driveRight(rightPower * turn_Speed);
             }
 
+            if(gamepad2.y) {
+                DriveMethods.startIntake(intake_Speed);
+            }
+            if(gamepad2.x){
+                DriveMethods.stopIntake();
+            }
+            if(gamepad2.a){
+                DriveMethods.startIntake(intake_Speed * -0.5);
+            }
+
+            if (liftPower > up_Thres)
+                DriveMethods.liftUp(lift_Speed * 0.5);
+
+            if (liftPower < down_Thres)
+                DriveMethods.liftDown(lift_Speed * -0.5);
+
+            if (gripPower > up_Thres)
+                DriveMethods.gripUp(grip_speed * 0.5);
+
+            if (gripPower < down_Thres)
+                DriveMethods.gripDown(grip_speed * -0.5);
+                 // mentions grip power
+
         }
 
-
-
+        }
     }
-}
