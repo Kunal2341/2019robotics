7fb50ed26bd63777f4cf7d4c9feb646d7a39db02
avamaryp <32110899+avamaryp@users.noreply.github.com>
2018-11-10 00:42:19 -0500

    Update TeleOpDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -9,6 +9,10 @@ public class TeleOpDrive extends LinearOpMode{
     HardwarePushBot robot = new
     HardwarePushBot();
 
+    //drive variables
+    //double PI = Math.PI;
+    //double power = 0.5 * Math.cos(PI*(-gamepad1.right_stick_y)-PI) + 0.5;
+    //double turn_Speed = -gamepad1.left_stick_x * 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -38,14 +42,14 @@ public class TeleOpDrive extends LinearOpMode{
             double up_Thres = 0.3;
             double down_Thres = -0.3;
 
-            double drive_Speed = 0.9;
+            double drive_Speed = 1.0;
             double turn_Speed = 0.5;
 
-            double intake_Speed = 0.5;         // mentions grip lift and intake
-            double lift_Speed = 0.5;
+            double intake_Speed = 0.7;         // mentions grip lift and intake
+            double lift_Speed = 1.0;
 
             double grip_speed = 0.5;
-
+//:)
             // Send calculated power to wheels
             // Drive system
             if (rightPower < forward_Thres && rightPower > reverse_Thres && leftPower < forward_Thres && leftPower > reverse_Thres) {
@@ -65,6 +69,41 @@ public class TeleOpDrive extends LinearOpMode{
                 DriveMethods.driveLeft(leftPower * turn_Speed);
                 DriveMethods.driveRight(rightPower * turn_Speed);
             }
+            //take out left power
+
+            /*if(-gamepad1.right_stick_y != 0) {
+                DriveMethods.driveRight(power);
+                DriveMethods.driveLeft(power);
+            }else if(-gamepad1.right_stick_x < 0){
+                DriveMethods.driveRight(turn_Speed);
+                DriveMethods.driveLeft(-turn_Speed);
+            }else if(-gamepad1.left_stick_x > 0){
+                 DriveMethods.driveRight(-turn_Speed);
+                DriveMethods.driveLeft(turn_Speed);
+            }else{
+                DriveMethods.stopRightMotors();
+                DriveMethods.stopLeftMotors();
+            }
+            */
+
+           /* if (power < forward_Thres && power > reverse_Thres && power < forward_Thres && power > reverse_Thres) {
+                DriveMethods.stopRightMotors();
+                DriveMethods.stopLeftMotors();
+            } else if (power != 0.0 && power != 0.0) {
+                DriveMethods.driveRight(power * drive_Speed);
+                DriveMethods.driveLeft(power * drive_Speed);
+            } else if (power != 0.0 && power == 0.0) {
+                DriveMethods.driveRight(power * drive_Speed);
+            } else if (power != 0.0 && power == 0.0) {
+                DriveMethods.driveLeft(power * drive_Speed);
+            } else if (power < reverse_Thres && power > forward_Thres) {
+                DriveMethods.driveLeft(power * turn_Speed);
+                DriveMethods.driveRight(power * turn_Speed);
+            } else if (power > forward_Thres && power < reverse_Thres) {
+                DriveMethods.driveLeft(power * turn_Speed);
+                DriveMethods.driveRight(power * turn_Speed);
+            }
+*/
 
             if(gamepad2.y) {
                 DriveMethods.startIntake(intake_Speed);
@@ -82,12 +121,19 @@ public class TeleOpDrive extends LinearOpMode{
             if (liftPower < down_Thres)
                 DriveMethods.liftDown(lift_Speed * -0.5);
 
+            if (liftPower == 0)
+                DriveMethods.stopLift();
+// what if youre not within threshold or at 0, stop
+
             if (gripPower > up_Thres)
                 DriveMethods.gripUp(grip_speed * 0.5);
 
             if (gripPower < down_Thres)
                 DriveMethods.gripDown(grip_speed * -0.5);
                  // mentions grip power
+            if (gripPower == 0)
+                DriveMethods.stopGrip();
+
 
         }
 
